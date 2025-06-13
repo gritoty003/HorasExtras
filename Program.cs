e27a12890ea4fb1f066bab78ce9aede381f47cf9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-13 19:25:40 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿﻿double salarioHora, horasTrabalhadas, horasExtras;
+
+Console.Clear();
+
+Console.WriteLine("--- Cálculo de Salário Mensal ---\n");
+
+
+Console.Write("informe o salario por hora: ");
+salarioHora = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("informe as horas trabalhadas: ");
+horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+        
+Console.Write("informe a quantidade de horas Extras: ");
+horasExtras = Convert.ToDouble(Console.ReadLine());
+
+
+ double salario = Calcular(salarioHora, horasTrabalhadas, horasExtras);
+        
+ Console.WriteLine("Salário total: R$ " + salario.ToString("F2"));
+    
+
+static double Calcular(double salarioHora, double horasTrabalhadas, double horasExtras)
+{
+double salario = salarioHora * horasTrabalhadas;
+double valorHorasExtra = salarioHora * 1.4;
+double salarioHorasExtras = valorHorasExtra * horasExtras;
+
+double salarioFinal = salario + salarioHorasExtras;
+return salarioFinal;
+        
+}
